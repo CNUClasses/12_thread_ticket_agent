47b2dd978bd27adba52667823ef4b20e09374e38
Keith Perkins <keith.perkins@cnu.edu>
2021-11-03 23:31:44 -0400

    Added theatre for people to go into

--- src/12_thread_ticket_agent.cpp
+++ src/12_thread_ticket_agent.cpp
@@ -9,21 +9,41 @@
 #include <time.h>
 #include <mutex>
 #include <chrono>
+#include <vector>
+#include "Theatre.h"
 
 using namespace std;
 
 //*************************************** global values, live for life of process*************************
 const int TOTAL_NUMB_TICKETS = 200000;
 const int TOTAL_TICKET_AGENTS =20;
+const int NUMB_THEATRES = 2;
 int numTickets = TOTAL_NUMB_TICKETS;	//accessible from anywhere if you use extern (ie extern numTickets; in other file)
 mutex m;
 mutex m_r;
+//set up the theatres	
+KP::Theatre threatre1(TOTAL_NUMB_TICKETS/NUMB_THEATRES);
+KP::Theatre threatre2(TOTAL_NUMB_TICKETS/NUMB_THEATRES);
+
 
 //because using m_r, can have 1 thread reporting while another 
 //decrements numTickets (increases concurrency)
-void report(int i){
+//void report(int i){
+//	lock_guard<mutex> lck(m_r);
+//	cout<<" agent "<<i<<" sold a ticket"<<endl;
+//}
+//because using m_r, can have 1 thread reporting while another 
+//decrements numTickets (increases concurrency)
+void report(int i, string s){
 	lock_guard<mutex> lck(m_r);
-	cout<<" agent "<<i<<" sold a ticket"<<endl;
+	cout<<" agent "<<i<<":"<<s<<endl;
+}
+
+bool enterTheatre(){
+	if(!threatre1.enter())
+		if(!threatre2.enter())
+			return false;
+	return true;
 }
 //sell until all gone
 void sellAsManyAsPossible( int agent){
@@ -31,27 +51,40 @@ void sellAsManyAsPossible( int agent){
 		//snooze for a bit, get at least 1 ms and yield the time slice to boot
 		//std::this_thread::sleep_for(std::chrono::milliseconds(1));
 		
-		report(agent);
-		
-		lock_guard<mutex> lck(m);
-		if(numTickets>0)
-			--numTickets;
-		else
-			break;		
+//		report(agent, "about to sell ticket");		
+		{
+			lock_guard<mutex> lck(m);
+			if(numTickets>0)
+				--numTickets;
+			else
+				break;
+		}
+
+		//choose a theatre
+		if(!enterTheatre())
+			report(agent, "OH NO! customer could not get in!");			
+		//if you have the following line and do not protect the theatre.enter() method
+		//then you get the right result for numPeopleInThreatre.  Comment out following line you do not
+//		report(agent, "sold a ticket");
 	}
 }
 
 int main()
-{
+{	
 	thread t1(sellAsManyAsPossible,1);
 	thread t2(sellAsManyAsPossible,2);
 	thread t3(sellAsManyAsPossible,3);
+	thread t4(sellAsManyAsPossible,4);
 
 	t2.join();
 	t1.join();
 	t3.join();
+	t4.join();
 	
-	cout<<endl<<"Number Tickets Remaining "<<numTickets<<endl;
+	cout<<"Number Tickets Remaining "<<numTickets<<endl;
+	
+	cout<<"theatre1 contains "<<threatre1.getnumPeopleInThreatre()<<" people"<<endl;
+	cout<<"theatre2 contains "<<threatre2.getnumPeopleInThreatre()<<" people"<<endl;
 	return 0;
 }
 
